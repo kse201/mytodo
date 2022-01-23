b7ff00485dd385f747ebfb53ce576281f46b17fc
kse201 <kse.201@gmail.com>
2022-01-23 22:24:13 +0900

    upgrade seed

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,32 +1,41 @@
-#[macro_use]
-extern crate seed;
-
-use futures::Future;
 use mytodo::{JsonApiResponse, Task};
-use seed::prelude::*;
-use seed::{fetch, Request};
+use seed::fetch;
+use seed::{prelude::*, *};
+
+/// Init
+fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
+    log!(url);
+    orders.perform_cmd(async { Msg::FetchedTasks(fetch_drills().await) });
+    Model { tasks: vec![] }
+}
+
+// Model
 
 struct Model {
     tasks: Vec<Task>,
 }
 
-#[derive(Clone, Debug)]
+// Msg
+
 enum Msg {
-    FetchedTasks(fetch::ResponseDataResult<JsonApiResponse>),
+    FetchedTasks(fetch::Result<JsonApiResponse>),
 }
 
+// Update
+
 fn update(msg: Msg, model: &mut Model, _orders: &mut impl Orders<Msg>) {
-    println!("foo");
     match msg {
         Msg::FetchedTasks(Ok(mut result)) => {
             model.tasks.clear();
             model.tasks.append(&mut result.data);
         }
         Msg::FetchedTasks(Err(reason)) => {
-            log!(format!("Error fetching: {:?}", reason));
+            log!(format!("error fetching: {:?}", reason));
         }
     }
 }
+
+// View
 
 fn view(model: &Model) -> impl View<Msg> {
     let tasks: Vec<Node<Msg>> = model
@@ -35,21 +44,23 @@ fn view(model: &Model) -> impl View<Msg> {
         .map(|t| li![{ t.title.clone() }])
         .collect();
 
-    h1![{ "Tasks" }, ul![tasks,],]
+    h1![{ "tasks" }, ul![tasks,],]
 }
 
-fn fetch_drills() -> impl Future<Item = Msg, Error = Msg> {
-    Request::new("http://localhost:8000/tasks/").fetch_json_data(Msg::FetchedTasks)
-}
-
-fn init(_url: Url, orders: &mut impl Orders<Msg>) -> Init<Model> {
-    println!("init");
-    orders.perform_cmd(fetch_drills());
-    Init::new(Model { tasks: vec![] })
+async fn fetch_drills() -> fetch::Result<JsonApiResponse> {
+    fetch("http://localhost:8000/tasks/")
+        .await?
+        .check_status()?
+        .json()
+        .await
 }
 
 #[wasm_bindgen(start)]
-pub fn render() {
-    println!("render");
-    seed::App::build(init, update, view).finish().run();
+pub fn start() {
+    console_error_panic_hook::set_once();
+    let root_element = document()
+        .get_elements_by_class_name("taskapp")
+        .item(0)
+        .expect("element with the class taskapp");
+    App::start(root_element, init, update, view);
 }
